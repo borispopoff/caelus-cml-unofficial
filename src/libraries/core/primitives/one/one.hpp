84c1add33fb11cdcca85a96d724e1a28fc2f2f7e
Darrin Stephens <d.stephens@appliedccm.com.au>
2019-10-02 13:24:19 +1000

    Foundation commit 774f76fc6, 36ac55382
    primitives/one: Corrected return type of '+' and '-' operators
    primitives::one: Specialize the '+' and '-' operators for scalar

--- src/libraries/core/primitives/one/one.hpp
+++ src/libraries/core/primitives/one/one.hpp
@@ -36,6 +36,10 @@ Description
 namespace CML
 {
 
+/*---------------------------------------------------------------------------*\
+                           Class one Declaration
+\*---------------------------------------------------------------------------*/
+
 class one
 {
 public:
@@ -78,6 +82,26 @@ public:
     typedef arg2 type;
 };
 
+inline scalar operator+(const scalar& t, const one&)
+{
+    return t + 1;
+}
+
+inline scalar operator+(const one&, const scalar& t)
+{
+    return 1 + t;
+}
+
+inline scalar operator-(const scalar& t, const one&)
+{
+    return t - 1;
+}
+
+inline scalar operator-(const one&, const scalar& t)
+{
+    return 1 - t;
+}
+
 inline const one& operator*(const one& o, const one&)
 {
     return o;
@@ -112,12 +136,46 @@ inline Type operator/(const one&, const Type& t)
     return scalar(1)/t;
 }
 
-
 template<class Type>
 inline const Type& operator/(const Type& t, const one&)
 {
     return t;
 }
+
+inline const one& min(const one& o, const one&)
+{
+    return o;
+}
+
+template<class Type>
+inline Type min(const one&, const Type& t)
+{
+    return min(scalar(1), t);
+}
+
+template<class Type>
+inline Type min(const Type& t, const one&)
+{
+    return min(t, scalar(1));
+}
+
+inline const one& max(const one& o, const one&)
+{
+    return o;
+}
+
+template<class Type>
+inline Type max(const one&, const Type& t)
+{
+    return max(scalar(1), t);
+}
+
+template<class Type>
+inline Type max(const Type& t, const one&)
+{
+    return max(t, scalar(1));
+}
+
 
 } // End namespace CML
 
