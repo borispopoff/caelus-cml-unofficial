3b148c88d8736a7e44b4d50fcbc0f2f09a9b4b17
Darrin Stephens <d.stephens@appliedccm.com.au>
2020-02-11 17:21:32 +1100

    WIP

--- bin/test/caelus_test_tools.py
+++ bin/test/caelus_test_tools.py
@@ -3,154 +3,319 @@
 from __future__ import print_function
 
 import os
+import struct
+import numpy as np
 from itertools import islice
-#import math
-#import fileinput
-#import re
-#import numpy
-#from xml.dom.minidom import parseString
-#from xml.dom.minidom import Document
-
-#try:
-#  import vtktools
-#except:
-#  pass
-
-#def parse_s(str):
-#    """Parse a .s file. Makes a dict vals that you use like:
-#    vals['maxp'][0] etc."""
-#
-#    vars = []
-#    vals = {}
-#    fieldsize = 13 # how big is each number in the list?
-#    fields_per_line = 12 # how many variables printed out per line
-#    no_lines = 1 # how many lines correspond to each timestep
-#    if os.stat(str)[6] == 0:
-#      raise Exception("Error: %s must not be empty!" % str)
-#
-#    f = open(str, "r")
-#    for line in f:
-#        if line.startswith("@@"):
-#            var_count = int(line[2:])
-#    f.close()
-#
-#    # OK. Fix for bizarre behaviour when compiled with Sun compiler.
-#    # I'm not going near study.F to try to find what causes this:
-#    # ugly hack time!
-#    expected_line_pattern = []
-#    if var_count <= fields_per_line: expected_line_pattern = [var_count]
-#    else:
-#      tmpvarcount = var_count
-#      while tmpvarcount > fields_per_line:
-#        expected_line_pattern.append(fields_per_line)
-#        tmpvarcount = tmpvarcount - fields_per_line
-#      if tmpvarcount > 0: expected_line_pattern.append(tmpvarcount)
-#
-#    # now let's see if it matches the expected pattern
-#    linecount = 0
-#    f = open(str, "r")
-#    actual_line_pattern = []
-#    for line in f:
-#      if not line.startswith("@"):
-#        linecount = linecount + 1
-#        actual_line_pattern.append(len(line[:-1]) / fieldsize)
-#
-#    f.close()
-#
-#    expected_line_pattern = expected_line_pattern * (linecount / len(expected_line_pattern))
-#
-#    no_lines = int(math.ceil(var_count / float(fields_per_line)))
-#    lines = []
-#
-#    f = open(str, "r")
-#
-#    for line in f:
-#        if line.startswith("@("):
-#            numnames = line.split() # (NUM):NAME
-#            for numname in numnames:
-#                name = numname.strip().split(':')[-1].lower() # I love python
-#                vars.append(name)
-#                vals[name] = []
-#        elif not line.startswith("@"):
-#            lines.append(line)
-#
-#    i = 0
-#    while i < len(lines):
-#        line = ""
-#        if actual_line_pattern[i] < expected_line_pattern[i]:
-#          print("Warning: .s file is not formatted as advertised. Skipping line %s." % i)
-#          i = i + 1
-#          continue
-#
-#        for j in range(i, i+no_lines):
-#            line += lines[j][:-1]
-#
-#        line += '\n'
-#        for var in vars:
-#            k = vars.index(var)
-#            try:
-#              vals[var].append(float(line[fieldsize*k:fieldsize*(k+1)]))
-#            except ValueError:
-#              num = line[fieldsize*k:fieldsize*(k+1)]
-#              reverse_num = num[::-1]
-#              reverse_num = reverse_num.replace('-', '-e', 1).replace('+', '+e', 1)
-#              vals[var].append(float(reverse_num[::-1]))
-#
-#        i += no_lines
-#
-#    return vals
-
-#if __name__ == "__main__":
-#    import sys
-#    var = parse_s(sys.argv[1])
-#    print(str(var))
-
-#def compare_variables(reference, current, error, zerotol=1.0e-14):
-#    """This takes in an array for a particular variable
-#    (e.g. kinetic energy) containing the values of that
-#    variable at the timesteps. It compares the output
-#    of the current run against a reference run,
-#    checking that the relative error is within the bound
-#    specified."""
-#
-#    assert len(reference) == len(current)
-#    relerrs = []
-#
-#    for i in range(len(current)):
-#       if abs(reference[i]) > zerotol: # decide if reference[i] is "0.0" or not
-#           diff = abs(reference[i] - current[i])
-#           relerr = (diff / abs(reference[i]))
-#           relerrs.append(relerr)
-#       else:
-#           relerrs.append(abs(current[i])) # not really a relative error but however
-#
-#    maxerr = max(relerrs)
-#    print("Asserting max relative error is smaller than", error)
-#    print("max relative error: %s; index: %s" % (maxerr, relerrs.index(maxerr)))
-#    assert maxerr < error
-#
-#def compare_variable(reference, current, error, zerotol=1.0e-14):
-#    """Compares current value with reference value. Relative error
-#    should be smaller than 'error'. If the reference is within
-#    zerotol however, an absolute error will be used."""
-#
-#    diff = abs(reference - current)
-#    if abs(reference) > zerotol: # decide if reference is "0.0" or not
-#       relerr = (diff / abs(reference))
-#       print("Asserting relative error is smaller than", error)
-#       print("relative error: " + str(relerr))
-#       assert(relerr < error)
-#    else:
-#       print("Asserting absolute error is smaller than", error)
-#       print("absolute error: "+ str(diff))
-#       assert(diff < error)
 
 
-def compare_output(current, base, skip):
+def CompareOutput(current, reference, skip):
   """
   Compare output files. Return True if they are identical an False otherwise.
   """
-  with open(current) as f1, open(base) as f2:
+  with open(current) as f1, open(reference) as f2:
     f1 = islice(f1, skip, None)
     f2 = islice(f2, skip, None)
     return all(x == y for x, y in zip(f1, f2))
+
+
+def CompareScalarRange(current, reference, tol):
+  """
+  Compare range for scalar fields. Return True if relative difference is less than 
+  tol otherwise False.
+  """
+  scMin, scMax = GetScalarRange(current)
+  srMin, srMax = GetScalarRange(reference)
+  scRange = scMax - scMin
+  srRange = srMax - srMin
+  
+  return abs(scRange - srRange)/srRange < tol
+
+
+def CompareVectorNormRange(current, reference, tol):
+  """
+  Compare range for norm of vector fields. Return True if relative difference is less than 
+  tol otherwise False.
+  """
+  vcMin, vcMax = GetVectorNormRange(current)
+  vrMin, vrMax = GetVectorNormRange(reference)
+  vcRange = vcMax - vcMin
+  vrRange = vrMax - vrMin
+  
+  return abs(vcRange - vrRange)/vrRange < tol
+
+
+def CompareFieldDiffRMS(current, reference, tol):
+  """
+  Compare RMS of difference between fields. Return True if RMS is less than 
+  tol otherwise False.
+  """
+  fRMS = DiffFieldRMS(current, reference)
+  
+  return fRMS < tol
+
+
+def GetScalarRange(name):
+  """Returns the range (min, max) of the specified scalar field."""
+  s = parse_internal_field(name)
+  minS = min(s)
+  maxS = max(s)
+  return minS, maxS
+
+
+def GetVectorNormRange(name):
+  """Return the range (min, max) of the norm of the specified vector field."""
+  v = parse_internal_field(name)
+  n = []
+
+  for x in range(0, v.shape[0]):
+      n.append(np.linalg.norm(v[x]))
+ 
+  np.array(n)
+  minN = min(n)
+  maxN = max(n)
+  return minN, maxN
+
+
+def DiffFieldRMS(f1name, f2name):
+  """
+  Taking the difference between two fields returning the RMS error
+  """
+  f1 = parse_internal_field(f1name)
+  f2 = parse_internal_field(f2name)
+
+  ferr = np.subtract(f1, f2)
+  return np.sqrt(np.mean(np.square(ferr)))
+
+
+"""
+The functions below have been copied from the OpenFOAM Python Parser written by
+XU Xianghua <dayigu at gmail dot com>
+
+Copyright (c) 2017 dayigu
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+def parse_field_all(fn):
+    """
+    parse internal field, extract data to numpy.array
+    :param fn: file name
+    :return: numpy array of internal field and boundary
+    """
+    if not os.path.exists(fn):
+        print("Can not open file " + fn)
+        return None
+    with open(fn, "rb") as f:
+        content = f.readlines()
+        return parse_internal_field_content(content), parse_boundary_content(content)
+
+
+def parse_internal_field(fn):
+    """
+    parse internal field, extract data to numpy.array
+    :param fn: file name
+    :return: numpy array of internal field
+    """
+    if not os.path.exists(fn):
+        print("Can not open file " + fn)
+        return None
+    with open(fn, "rb") as f:
+        content = f.readlines()
+        return parse_internal_field_content(content)
+
+
+def parse_internal_field_content(content):
+    """
+    parse internal field from content
+    :param content: contents of lines
+    :return: numpy array of internal field
+    """
+    is_binary = is_binary_format(content)
+    for ln, lc in enumerate(content):
+        if lc.startswith(b'internalField'):
+            if b'nonuniform' in lc:
+                return parse_data_nonuniform(content, ln, len(content), is_binary)
+            elif b'uniform' in lc:
+                return parse_data_uniform(content[ln])
+            break
+    return None
+
+
+def parse_boundary_field(fn):
+    """
+    parse internal field, extract data to numpy.array
+    :param fn: file name
+    :return: numpy array of boundary field
+    """
+    if not os.path.exists(fn):
+        print("Can not open file " + fn)
+        return None
+    with open(fn, "rb") as f:
+        content = f.readlines()
+        return parse_boundary_content(content)
+
+
+def parse_boundary_content(content):
+    """
+    parse each boundary from boundaryField
+    :param content:
+    :return:
+    """
+    data = {}
+    is_binary = is_binary_format(content)
+    bd = split_boundary_content(content)
+    for boundary, (n1, n2) in bd.items():
+        pd = {}
+        n = n1
+        while True:
+            lc = content[n]
+            if b'nonuniform' in lc:
+                v = parse_data_nonuniform(content, n, n2, is_binary)
+                pd[lc.split()[0]] = v
+                if not is_binary:
+                    n += len(v) + 4
+                else:
+                    n += 3
+                continue
+            elif b'uniform' in lc:
+                pd[lc.split()[0]] = parse_data_uniform(content[n])
+            n += 1
+            if n > n2:
+                break
+        data[boundary] = pd
+    return data
+
+
+def parse_data_uniform(line):
+    """
+    parse uniform data from a line
+    :param line: a line include uniform data, eg. "value           uniform (0 0 0);"
+    :return: data
+    """
+    if b'(' in line:
+        return np.array([float(x) for x in line.split(b'(')[1].split(b')')[0].split()])
+    return float(line.split(b'uniform')[1].split(b';')[0])
+
+
+def parse_data_nonuniform(content, n, n2, is_binary):
+    """
+    parse nonuniform data from lines
+    :param content: data content
+    :param n: line number
+    :param n2: last line number
+    :param is_binary: binary format or not
+    :return: data
+    """
+    num = int(content[n + 1])
+    if not is_binary:
+        if b'scalar' in content[n]:
+            data = np.array([float(x) for x in content[n + 3:n + 3 + num]])
+        else:
+            data = np.array([ln[1:-2].split() for ln in content[n + 3:n + 3 + num]], dtype=float)
+    else:
+        nn = 1
+        if b'vector' in content[n]:
+            nn = 3
+        elif b'symmtensor' in content[n]:
+            nn = 6
+        elif b'tensor' in content[n]:
+            nn = 9
+        buf = b''.join(content[n+2:n2+1])
+        vv = np.array(struct.unpack('{}d'.format(num*nn),
+                                    buf[struct.calcsize('c'):num*nn*struct.calcsize('d')+struct.calcsize('c')]))
+        if nn > 1:
+            data = vv.reshape((num, nn))
+        else:
+            data = vv
+    return data
+
+
+def split_boundary_content(content):
+    """
+    split each boundary from boundaryField
+    :param content:
+    :return: boundary and its content range
+    """
+    bd = {}
+    n = 0
+    in_boundary_field = False
+    in_patch_field = False
+    current_path = ''
+    while True:
+        lc = content[n]
+        if lc.startswith(b'boundaryField'):
+            in_boundary_field = True
+            if content[n+1].startswith(b'{'):
+                n += 2
+                continue
+            elif content[n+1].strip() == b'' and content[n+2].startswith(b'{'):
+                n += 3
+                continue
+            else:
+                print('no { after boundaryField')
+                break
+        if in_boundary_field:
+            if lc.rstrip() == b'}':
+                break
+            if in_patch_field:
+                if lc.strip() == b'}':
+                    bd[current_path][1] = n-1
+                    in_patch_field = False
+                    current_path = ''
+                n += 1
+                continue
+            if lc.strip() == b'':
+                n += 1
+                continue
+            current_path = lc.strip()
+            if content[n+1].strip() == b'{':
+                n += 2
+            elif content[n+1].strip() == b'' and content[n+2].strip() == b'{':
+                n += 3
+            else:
+                print('no { after boundary patch')
+                break
+            in_patch_field = True
+            bd[current_path] = [n,n]
+            continue
+        n += 1
+        if n > len(content):
+            if in_boundary_field:
+                print('error, boundaryField not end with }')
+            break
+
+    return bd
+
+
+def is_binary_format(content, maxline=20):
+    """
+    parse file header to judge the format is binary or not
+    :param content: file content in line list
+    :param maxline: maximum lines to parse
+    :return: binary format or not
+    """
+    for lc in content[:maxline]:
+        if b'format' in lc:
+            if b'binary' in lc:
+                return True
+            return False
+    return False
