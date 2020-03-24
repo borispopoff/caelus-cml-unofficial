5c9be043fe22b0e4599fad42f93b037527ce03c6
Darrin Stephens <d.stephens@appliedccm.com.au>
2020-03-24 12:59:57 +1100

    Add some surface mesh utilities

---
+++ src/libraries/core/fields/Fields/triadField/triadField.hpp
@@ -0,0 +1,59 @@
+/*---------------------------------------------------------------------------*\
+Copyright (C) 2011-2012 Caelus Foundation
+-------------------------------------------------------------------------------
+License
+    This file is part of Caelus.
+
+    Caelus is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Caelus is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Caelus.  If not, see <http://www.gnu.org/licenses/>.
+
+Typedef
+    CML::triadField
+
+Description
+    Specialisation of Field\<T\> for triad.
+
+SourceFiles
+    triadField.cpp
+
+\*---------------------------------------------------------------------------*/
+
+#ifndef triadField_HPP
+#define triadField_HPP
+
+#include "Field.hpp"
+#include "triad.hpp"
+
+#define TEMPLATE
+#include "FieldFunctionsM.hpp"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace CML
+{
+
+typedef Field<triad> triadField;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace CML
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#include "undefFieldFunctionsM.hpp"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif
+
+// ************************************************************************* //
